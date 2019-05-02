ce6f0fd040a180de4b59d9badcb4e893c9638a41
Cody Frank <cody.frank30@gmail.com>
2019-05-02 23:41:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,58 @@
 # Write your code here.
+require 'pry'
+# instead of manes, we'll just use numbers
+# all methods should still work even with no arguments
+# the first number on the reel is 345
+# current the line is [343, 344]
+$katz_deli = [343, 344]
+def line
+  if $katz_deli.empty?
+    puts "The line is currently empty."
+  else
+    line = "The line is currently:"
+    $katz_deli.each_with_index do |name, index|
+      line.insert(-1," #{index + 1}. #{name}")
+    end
+    puts line
+  end
+
+end
+
+def take_a_number
+  katz_deli.push(new_customer)
+  puts "Welcome, #{new_customer}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving
+  if !katz_deli.empty?
+    puts "Currently serving #{katz_deli.shift}."
+  else puts "There is nobody waiting to be served!"
+  end
+end
+
+binding.pry
+
+# def line(katz_deli)
+#   if katz_deli.empty?
+#     puts "The line is currently empty."
+#   else
+#     line = "The line is currently:"
+#     katz_deli.each_with_index do |name, index|
+#       line.insert(-1," #{index + 1}. #{name}")
+#     end
+#     puts line
+#   end
+#
+# end
+#
+# def take_a_number(katz_deli, new_customer)
+#   katz_deli.push(new_customer)
+#   puts "Welcome, #{new_customer}. You are number #{katz_deli.length} in line."
+# end
+#
+# def now_serving(katz_deli)
+#   if !katz_deli.empty?
+#     puts "Currently serving #{katz_deli.shift}."
+#   else puts "There is nobody waiting to be served!"
+#   end
+# end
